c7732652f7e0b56e2e2fca2a518896275f009961
etenprod <90542583+etenprod@users.noreply.github.com>
2022-03-19 11:42:11 +0200

    Update index.php

--- etenwrite/index.php
+++ etenwrite/index.php
@@ -5,8 +5,19 @@ if($_SERVER['REQUEST_URI'] == '/') {
 $Page = 'index';
 $Module = 'index';
 } else {
-$URL_Path = parse_url
+$URL_Path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$URL_Parts = explode('/', trim($URL_Path, ' /'));
+$Page = array_shift($URL_Parts);
+$Module = array_shift($URL_Parts);
 
+if (!empty($Module)) {
+$Param = array();
+for ($i = 0; $i < count($URL_Parts); $i++) {
+$Param[$URL_Parts[$i]] = $URL_Parts[++$i];
+}
+}  
+}  
+ 
 
-
+if($Page=='index')include('page/index.php');
 ?>
