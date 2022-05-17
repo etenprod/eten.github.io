c0557f0ac50e31ad34af89c189a27dc2bdaa80bd
etenprod <90542583+etenprod@users.noreply.github.com>
2022-05-17 08:49:09 +0300

    Delete minecode directory

--- minecode/js/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["/rules/farm","/rules/mods","/wiki/guide/chat","/wiki/guide/emotes","/wiki/guide/heads","/wiki/guide/itemedit","/wiki/guide/tns","/wiki/ws/replenish"]),self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB();
