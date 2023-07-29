95dfa73362786935a929445a532e7eaeccdcb065
David <87504880+darud63@users.noreply.github.com>
2023-07-30 02:15:24 +0300

    Update bounds.js

--- hyperloop2/js/bounds.js
+++ hyperloop2/js/bounds.js
@@ -1,7 +1,4 @@
 var kingdoms={
 	type:"FeatureCollection",features:[
 	{type:"Feature",properties:{bg:"#F85559",name:"1"},geometry:{type:"MultiPolygon",coordinates:[[[[5892,2692],[5892,2575],[6043,2575],[6043,2692]]]]}},
-	{type:"Feature",properties:{bg:"#5C52CF",name:"Банк"},geometry:{type:"MultiPolygon",coordinates:[[[[6540,1445],[6248,1445],[6248,1710],[6540,1710]]]]}},
-		{ type: "Feature", properties: { bg: "#46AF6E", name: "Портал в енд | 256" }, geometry: { type: "MultiPolygon", coordinates: [[[[1276, 1024], [1284, 1024], [1284, 893], [1276, 893]]]] } },
-		{ type: "Feature", properties: { bg: "#FEC90B", name: "Карпатія | -81" }, geometry: { type: "MultiPolygon", coordinates: [[[[939, 1024], [947, 1024], [947, 993], [939, 993]]]] } },
-		{ type: "Feature", properties: { bg: "#5C52CF", name: "Левант | -180" }, geometry: { type: "MultiPolygon", coordinates: [[[[1094, 840], [1094, 848], [1024, 848], [1024, 840]]]]}}]};
+	{type:"Feature",properties:{bg:"#5C52CF",name:"Банк"},geometry:{type:"MultiPolygon",coordinates:[[[[6540,1445],[6248,1445],[6248,1710],[6540,1710]]]]}}]};
